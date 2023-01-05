b0ce736bd03279e8d222a18fc2d9838bb634eff8
Matondo <matondojoaokitemoco@gmail.com>
2023-01-05 14:46:32 +0100

    Adicionando os campos que serão retornados no Recurso de Manipulação de Imagem

--- app/Http/Resources/V1/ImageManipulationResource.php
+++ app/Http/Resources/V1/ImageManipulationResource.php
@@ -14,6 +14,16 @@ class ImageManipulationResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return[
+            'id'=>$this->id,
+            'name'=>$this->name,
+            'path'=>$this->path,
+            'type'=>$this->type,
+            'data'=>$this->data,
+            'output_path'=>$this->data,
+            'album_id'=>$this->data,
+            'created_at'=>$this->created_at,
+            'updated_at'=>$this->updated_at,
+        ];
     }
 }
